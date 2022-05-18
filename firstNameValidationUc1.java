c779d8f87df3bf41db1022303f6f2018c7cb055c
Rajudv19 <rajudv1997@gmail.com>
2022-05-18 09:24:40 +0530

    Added code to Validate FirstName

---
+++ firstNameValidationUc1.java
@@ -0,0 +1,29 @@
+package com.BridgeLabs.day20;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class firstNameValidationUc1 {
+	private static void ValidatinguserFirstName() {
+
+		//UC1 User need to enter valid First Name
+		String firstNamePattern = "[A-Z]{1}[a-z]{2,}";
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter First Name:");
+		String firstName = scanner.next();
+
+		Pattern pattern = Pattern.compile(firstNamePattern);
+		Matcher matcher = pattern.matcher(firstName);
+		boolean result = matcher.matches();
+
+		if(result == true)
+			System.out.println("First Name is Valid");
+		else
+			System.out.println("First Name is Invalid");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to User Registration program");
+		ValidatinguserFirstName();
+	}
+}
